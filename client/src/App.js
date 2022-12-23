52394de1ce1a1be5c6e8143997764ccb3dcd243e
tanarakchuns <tanarak.chuns@gmail.com>
2022-12-23 15:42:34 +0700

    add login

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes, Link, Navigate } from "react-router-dom";
 import "./App.css";
 
 import Index from "./components/Index/Index";
-
+import Login from "./components/Login/Login";
 import React from "react";
 
 function App() {
@@ -12,11 +12,13 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </main>
       <footer>
         <div>
           <Link to="/">Test Index</Link>
+          <Link to="/login">Test Login</Link>
         </div>
       </footer>
     </div>
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import axios from "axios";
+
+/* eslint-disable react-hooks/exhaustive-deps */
+const Login = () => {
+  return <h1>test Login</h1>;
+};
+
+export default Login;
