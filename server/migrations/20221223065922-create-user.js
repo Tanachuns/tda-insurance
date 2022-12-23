81bc448a2927c4c062c983bd2bcc1371e5877adc
tanarakchuns <tanarak.chuns@gmail.com>
2022-12-23 14:14:51 +0700

    init database

---
+++ server/migrations/20221223065922-create-user.js
@@ -0,0 +1,48 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstname: {
+        type: Sequelize.STRING,
+      },
+      lastname: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      tel: {
+        type: Sequelize.STRING,
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
---
+++ server/seeders/20221223071235-demo-users.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */ await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstname: "test",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */ await queryInterface.bulkDelete("Users", null, {});
+  },
+};
