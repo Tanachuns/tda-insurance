fcadda4dac3181a1d4a921f11b5e36feab2a7eb3
maharnnop <maharnnop.s@gmail.com>
2022-12-26 11:13:14 +0700

    add route& link admin,signup

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,29 @@ import Index from "./components/Index/Index";
 import Login from "./components/Login/Login";
 import React from "react";
 import Car from "./components/Car/Car";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <div>
+          <Link to="/">Test Index</Link>
+          <Link to="/admin">--ADmin--</Link>
+          <Link to="/signup">--SignUp--</Link>
+          <Link to="/login">Test Login</Link>
+          <Link to="/car">Test Car</Link>
+        </div>
+      </header>
       <main>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/car" element={<Car />} />
+          <Route path="/admin"  />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </main>
-      <footer>
-        <div>
-          <Link to="/">Test Index</Link>
-          <Link to="/login">Test Login</Link>
-          <Link to="/car">Test Car</Link>
-        </div>
-      </footer>
     </div>
   );
 }
