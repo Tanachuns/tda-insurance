adcc1d37c2c258fc7d78803de31992b200496003
JoaNattapon <joenattapon.sangchai@gmail.com>
2022-12-26 11:49:18 +0700

    Created  Login form ver 1.0

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import axios from "axios";
+import { BrowserRouter, Routes, Route, Link, Navigation } from "react-router-dom";
 
 /* eslint-disable react-hooks/exhaustive-deps */
+const url = 'http://localhost:3002/auth';
+
 const Login = () => {
-  return <h1>test Login</h1>;
+  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sentUsername = () => {
+
+    axios
+    .put(url)
+    .then((res) => {
+      console.log(res.data);
+      setUsername(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  const sentPassword = () => {
+    axios
+    .put(url)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  sentUsername();
+  sentPassword();
+
+  return (
+    <div>
+      <h1>test Login</h1>
+      <h2>Log in</h2>
+
+      <form>
+        <input type="text" placeholder="Username" value={username}
+        onChange={(e) => setUsername(e.target.value)}/>
+        <input type="text" placeholder="Password" value={password}
+        onChange={(e) => setPassword(e.target.value)}/>
+        <button >Login</button>
+      </form>
+
+    </div>
+
+  )
 };
 
 export default Login;
