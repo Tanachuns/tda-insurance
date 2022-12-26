771eeb2c7f755272198bc91ffdb64f0686d3cd20
JoaNattapon <joenattapon.sangchai@gmail.com>
2022-12-26 13:59:52 +0700

    Edited log in form ver 2.0

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -43,7 +43,7 @@ const Login = () => {
       <form>
         <input type="text" placeholder="Username" value={username}
         onChange={(e) => setUsername(e.target.value)}/>
-        <input type="text" placeholder="Password" value={password}
+        <input type="password" placeholder="Password" value={password}
         onChange={(e) => setPassword(e.target.value)}/>
         <button >Login</button>
       </form>
