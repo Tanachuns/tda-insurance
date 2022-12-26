2c3268d2a56bde8a747fd0a1b6f2dac47ff77792
tanarakchuns <tanarak.chuns@gmail.com>
2022-12-26 17:19:50 +0700

    add profile page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-<<<<<<< HEAD
 import {
   BrowserRouter,
   Routes,
@@ -8,10 +7,12 @@ import {
   Link,
   Navigation,
 } from "react-router-dom";
-=======
-import { BrowserRouter, Routes, Route, Link, Navigation } from "react-router-dom";
-import  { Header, InputBtn, LoginBtn, NormalText }  from "../StylesPages/LoginStyles";
->>>>>>> b951863b1c01699c701d5db6d43ef7ac33f152df
+import {
+  Header,
+  InputBtn,
+  LoginBtn,
+  NormalText,
+} from "../StylesPages/LoginStyles";
 
 /* eslint-disable react-hooks/exhaustive-deps */
 const url = "http://localhost:3002/auth";
@@ -21,62 +22,39 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
-<<<<<<< HEAD
     e.preventDefault();
-  };
-=======
-
-    e.preventDefault()
     axios
-    .post('')
-    .then((res) => {
-
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
-  
->>>>>>> b951863b1c01699c701d5db6d43ef7ac33f152df
+      .post("")
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
       <Header>Welcome Back !</Header>
 
       <form onSubmit={handleSubmit}>
-<<<<<<< HEAD
-        <input
+        <InputBtn
           type="text"
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />{" "}
         <br />
-        <input
+        <InputBtn
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />{" "}
         <br />
-        <button type="submit">Login</button>
-      </form>
-      <Link to="/signup">First time here ? Let's sign up</Link>
-=======
-        
-        <InputBtn type="text" placeholder="Username" value={username}
-        onChange={(e) => setUsername(e.target.value)}/> <br/>
-        <InputBtn type="password" placeholder="Password" value={password}
-        onChange={(e) => setPassword(e.target.value)}/> <br/>
-
         <LoginBtn type="submit">Login</LoginBtn>
-        
       </form>
       <NormalText>
         <Link to="/signup">First time here ? Let's sign up</Link>
       </NormalText>
-
->>>>>>> b951863b1c01699c701d5db6d43ef7ac33f152df
     </div>
   );
 };
