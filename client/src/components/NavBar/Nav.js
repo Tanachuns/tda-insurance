4e0ca6d6de681c7f9f744d51d14902770ac96ec0
maharnnop <maharnnop.s@gmail.com>
2022-12-27 15:24:58 +0700

    fix bug nav

--- client/src/components/NavBar/Nav.js
+++ client/src/components/NavBar/Nav.js
@@ -52,7 +52,28 @@ function Nav() {
       </>
     );
   }
-  return <div>Test</div>;
+  return (
+    <>
+      {/* Use components from NavBar-Style */}
+      <NavBar showToggle={showToggle}>
+        <Bars onClick={() => setShowToggle(!showToggle)} />
+        <NavLogo to="/">
+          <img
+            style={{ height: "70px" }}
+            src="https://drive.google.com/uc?id=1C_LXEXZfAW3s7UwHXST5GZu8iEJX0zAf&authuser=0"
+          />
+        </NavLogo>
+        <NavMenu showToggle={showToggle}>
+          <NavList>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </NavList>
+          <NavList>
+            <NavLink to="/login">Login</NavLink>
+          </NavList>
+        </NavMenu>
+      </NavBar>
+    </>
+  );
 }
 
 export default Nav;
