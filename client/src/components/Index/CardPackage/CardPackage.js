96452204758ab89a642e8709d9296e76de62defb
BelleZarocha <sarocha.pj@gmail.com>
2022-12-27 14:36:59 +0700

    Style Card Index

--- client/src/components/Index/CardPackage/CardPackage.js
+++ client/src/components/Index/CardPackage/CardPackage.js
@@ -2,20 +2,28 @@ import React, { useState } from "react"
 // import axios from "axios"
 import {Link} from "react-router-dom";
 import './CardPackage.css'
+import { StyledBox, CardHeader, CardBody, StyledImage } from "../../StylesPages/CardPackage"
+
+
+
 
 const CardPackage = props => {
-
-    const [packageData, setPackageData] = useState({})
-
-
-    return (
-        <Link to={`/packages/${props.detail.id}`}>
-        <fieldset>
-            <h2>Package :{props.detail.name}</h2>
-            <h2>Cost :{props.detail.cost}</h2>
-        </fieldset>
-        </Link>
-    )
+    return (                
+        <StyledBox>
+            <CardHeader>
+                <StyledImage src={props.detail.picture}/>
+            </CardHeader>
+            <CardBody>
+                <Link to={`/packages/${props.detail.id}`}>
+                
+                    <h2>Package :{props.detail.name}</h2>
+                    <h2>Cost :{props.detail.cost}</h2>
+    
+                </Link>
+            </CardBody>
+            
+        </StyledBox>
+    )    
 }
 
 export default CardPackage
